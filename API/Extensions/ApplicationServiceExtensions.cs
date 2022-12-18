9f74e6e8fadc886b40bf07aca26d689e2a4b1d78
Saber Ghasempour <saber.ghasempour@gmail.com>
2022-12-18 15:37:08 +0300

    Add Extensions for Services

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Application.Cities;
+using Application.Core;
+using Application.Interfaces;
+using Application.Provinces;
+using Infrastructure.Email;
+using Infrastructure.Security;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using System.Collections.Generic;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv7", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithExposedHeaders("WWW-Authenticate", "Pagination")
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(typeof(ListCities.Handler).Assembly);
+            services.AddMediatR(typeof(FullListCities.Handler).Assembly);
+            services.AddMediatR(typeof(ListProvinces.Handler).Assembly);
+            services.AddMediatR(typeof(FullListProvinces.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<EmailSender>();
+            services.AddSignalR();
+
+            return services;
+        }
+    }
+}
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,88 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Persistence;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Infrastructure.Security;
+using Domain.Constant;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddEntityFrameworkStores<DataContext>()
+                .AddSignInManager<SignInManager<User>>()
+                .AddDefaultTokenProviders();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    opt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy(Constants.AdminRole, policy =>
+                {
+                    policy.Requirements.Add(new IsAdminRequirement());
+                });
+                opt.AddPolicy(Constants.ManagerRole, policy =>
+                {
+                    policy.Requirements.Add(new IsManagerRequirement());
+                });
+                opt.AddPolicy(Constants.CashierRole, policy =>
+                {
+                    policy.Requirements.Add(new IsCashierRequirement());
+                });
+                opt.AddPolicy(Constants.WaiterRole, policy =>
+                {
+                    policy.Requirements.Add(new IsWaiterRequirement());
+                });
+            });
+            services.AddTransient<IAuthorizationHandler, IsAdminRequirementHandler>();
+            services.AddTransient<IAuthorizationHandler, IsManagerRequirementHandler>();
+            services.AddTransient<IAuthorizationHandler, IsCashierRequirementHandler>();
+            services.AddTransient<IAuthorizationHandler, IsWaiterRequirementHandler>();
+            services.AddScoped<TokenService>();
+
+            return services;
+        }
+    }
+}
