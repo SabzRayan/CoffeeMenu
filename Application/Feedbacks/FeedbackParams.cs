d8fc2d972ad8d9dbea02c53820785cfdc838b09f
Saber Ghasempour <saber.ghasempour@gmail.com>
2022-12-25 22:57:28 +0300

    Add Feedback Controller

--- Application/Feedbacks/FeedbackParams.cs
+++ Application/Feedbacks/FeedbackParams.cs
@@ -9,6 +9,7 @@ namespace Application.Feedbacks
         public Guid? OrderId { get; set; }
         public Guid? ProductId { get; set;}
         public HappinessEnum? Happiness { get; set; }
+        public Guid? BranchId { get; set; }
         public bool ShowDeleted { get; set; }
     }
 }
