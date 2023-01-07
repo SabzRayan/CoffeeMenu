cbdd6951a9e7d2bacdcbcfcfa5a7f2d084db40a7
Saber Ghasempour <saber.ghasempour@gmail.com>
2023-01-07 20:40:12 +0300

    Add Chef suggestion to product model

---
+++ Persistence/Migrations/20230101202719_ChefSuggestionInProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChefSuggestionInProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ChefSuggestion",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChefSuggestion",
+                table: "Products");
+        }
+    }
+}
